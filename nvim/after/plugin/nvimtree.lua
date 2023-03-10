4d1f47cd891ec56010cdde49ffa1c36e9b0ff04e
PasNinii <nicrannou@gmail.com>
2023-03-10 13:53:29 +0100

    fix: config

--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -4,7 +4,7 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
-  disable_netrw=false,
+  disable_netrw = false,
   sort_by = "case_sensitive",
   renderer = {
     group_empty = true,
@@ -14,3 +14,9 @@ require("nvim-tree").setup({
   },
 })
 
+local function open_nvim_tree()
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
