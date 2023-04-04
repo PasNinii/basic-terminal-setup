93d34a8b07b17399bba58f93d4d26df00dc9d670
PasNinii <nicrannou@gmail.com>
2023-04-04 10:28:57 +0200

    fix: nvchad config update

--- nvim_config_setup.sh
+++ nvim_config_setup.sh
@@ -1,5 +1,5 @@
 # rm -rf $HOME/.config/nvim
 # ln -s $HOME/workspace/nvim/nvim $HOME/.config/nvim
 
-rm -rf $HOME/.config/nvim/lua/custom/*
-ln -s $HOME/workspace/nvim/nvchad_custom $HOME/.config/nvim/lua/custom
+rm -rf $HOME/.config/nvim/lua/custom
+ln -s $HOME/workspace/basic-terminal-setup/nvchad_custom $HOME/.config/nvim/lua/custom
