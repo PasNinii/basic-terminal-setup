9016b66eacd21b45174fc42283fa04da12ba1b94
PasNinii <nicrannou@gmail.com>
2023-04-29 01:57:30 +0200

    feat: scrolloff

--- custom/init.lua
+++ custom/init.lua
@@ -1,8 +1,10 @@
 vim.g.copilot_assume_mapped = true
 vim.opt.guicursor = ""
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
 
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
