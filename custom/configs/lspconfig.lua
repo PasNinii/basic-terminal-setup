951a1fe360beee5e4b45aa6f08927eb618645d88
PasNinii <nicrannou@gmail.com>
2023-06-01 18:31:45 +0000

    chore: nvim config

--- custom/configs/lspconfig.lua
+++ custom/configs/lspconfig.lua
@@ -5,13 +5,7 @@ local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
 local servers = {
-  "angularls",
-  "cssls",
-  "eslint",
-  "intelephense",
   "lua_ls",
-  "pyright",
-  "tsserver",
 }
 
 for _, lsp in ipairs(servers) do
