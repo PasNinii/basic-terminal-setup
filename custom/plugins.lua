d8011a67bcff036b4063b020d981d9d549f290ce
PasNinii <nicrannou@gmail.com>
2023-06-10 09:46:56 +0200

    config update

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -1,5 +1,26 @@
 local plugins = {
   {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+  {
     "lewis6991/gitsigns.nvim",
     opts = {
       current_line_blame = true,
