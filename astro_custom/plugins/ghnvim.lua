900666d08b39248597230cfb2f51578a350bf72c
PasNinii <nicrannou@gmail.com>
2023-12-29 13:30:19 +0100

    misc: PR plugin

---
+++ astro_custom/plugins/ghnvim.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"ldelossa/gh.nvim",
+		lazy=false,
+		dependencies = {
+			{
+				"ldelossa/litee.nvim",
+				config = function()
+					require("litee.lib").setup()
+				end,
+			},
+		},
+		config = function()
+			require("litee.gh").setup()
+		end,
+	},
+}
