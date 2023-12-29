48907e1d2a1ca4a53ad45ca85cd6a58cbb9b78da
PasNinii <nicrannou@gmail.com>
2023-12-29 14:20:03 +0100

    update(aliases)

--- ubuntu_packet_setup.sh
+++ ubuntu_packet_setup.sh
@@ -1,3 +1,5 @@
+current_path=$(pwd)
+
 sudo apt install ripgrep zsh tmux curl -y
 
 rm -rf ~/.config/nvim
@@ -5,3 +7,6 @@ rm -rf ~/.local/share/nvim
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo source $current_path/term_config >> $HOME/.zshrc
+cp $current_path/tmux.conf $HOME/.tmux.conf
